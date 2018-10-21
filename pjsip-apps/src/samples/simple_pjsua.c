4241e526dd2637bade4d455516a4e0f3a58ec1e4
Andrew <bandrew.work@gmail.com>
2018-10-21 22:28:23 +0700

    translog

--- pjsip-apps/src/samples/simple_pjsua.c
+++ pjsip-apps/src/samples/simple_pjsua.c
@@ -758,7 +758,7 @@ static int callback_test(struct lws* wsi, enum lws_callback_reasons reason, void
 	case LWS_CALLBACK_CLIENT_RECEIVE:
 		// printf("callback_test LWS_CALLBACK_CLIENT_RECEIVE.\n");
 		{
-			printf("[Test Protocol] Received data: \"%s\"\n", (char*)in);
+			printf("[Test Protocol %d] Received data: \"%s\"\n", (char*)in, call_index);
 			// Parse JSON
 			json_char* json;
         	json_value* value;
